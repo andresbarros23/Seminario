c4eebe9103dad5414b9db4d071c2d775b1d77cf1
Andres <abarros@belatrix.com>
2019-12-11 17:47:27 -0300

    First commit

---
+++ TournamentTracker/TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        public string FirstName { get; set; }
+        public string EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<MatchupModel> Rounds { get; set; } = new List<MatchupModel>();            
+    }
+}
